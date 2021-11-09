a016b4dc54ba97d43533ee33d6a0706c0c9cf541
Mujahidul Islam <mislam171020@bscse.uiu.ac.bd>
2021-11-09 12:58:28 +0600

    chat ui update

--- mainchatapp/urls.py
+++ mainchatapp/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url
 from . import views
 from .views import ChatterBotAppView
 urlpatterns = [
-    path('landing/', ChatterBotAppView.as_view(), name='main'),
-    path('', views.index, name='index'),
+    path('', ChatterBotAppView.as_view(), name='main'),
     path('room', views.room, name='room'),
 ]
